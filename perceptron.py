30be5d1ce950a292a0e951becf9993daee9c193b
Eddie Xie <oeddyo@gmail.com>
2023-03-17 21:22:00 -0700

    Perceptron completed

--- perceptron.py
+++ perceptron.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 
 class AndPerceptron:
@@ -13,13 +14,46 @@ class AndPerceptron:
             return False
 
 
+class NotAndPerceptron:
+    def __init__(self):
+        self.p = AndPerceptron()
+
+    def pred(self, x1, x2):
+        return not self.p.pred(x1, x2)
+
+
 class OrPerceptron:
     def __init__(self):
         self.w1 = 0.6
         self.w2 = 0.6
         self.threshold = 0.5
+
     def pred(self, x1, x2):
 
         if self.w1 * x1 + self.w2 * x2 > self.threshold:
             return True
         return False
+
+
+class AndPerceptronNumpy:
+    def __init__(self):
+        self.w = np.array([0.5, 0.5])
+        self.b = -0.7
+
+    def pred(self, x1, x2):
+        v = np.array([x1, x2])
+        return np.sum(v * self.w) + self.b > 0
+
+
+class XorPerceptronNumpy:
+    def __init__(self):
+        self.and_gate = AndPerceptronNumpy()
+        self.or_gate = OrPerceptron()
+        self.not_and_gate = NotAndPerceptron()
+
+    def pred(self, x1, x2):
+        r1 = int(self.not_and_gate.pred(x1, x2))
+        r2 = int(self.or_gate.pred(x1, x2))
+
+        return self.and_gate.pred(r1, r2)
+
--- test_perceptron.py
+++ test_perceptron.py
@@ -1,6 +1,9 @@
 import unittest
 from perceptron import AndPerceptron
+from perceptron import NotAndPerceptron
+from perceptron import AndPerceptronNumpy
 from perceptron import OrPerceptron
+from perceptron import XorPerceptronNumpy
 
 
 class TestPerceptron(unittest.TestCase):
@@ -11,6 +14,13 @@ class TestPerceptron(unittest.TestCase):
         self.assertEqual(p.pred(0, 1), False)
         self.assertEqual(p.pred(0, 0), False)
 
+    def test_not_and(self):
+        p = NotAndPerceptron()
+        self.assertEqual(p.pred(1, 1), False)
+        self.assertEqual(p.pred(1, 0), True)
+        self.assertEqual(p.pred(0, 1), True)
+        self.assertEqual(p.pred(0, 0), True)
+
     def test_or_gate(self):
         p = OrPerceptron()
 
@@ -19,3 +29,16 @@ class TestPerceptron(unittest.TestCase):
         self.assertEqual(p.pred(0, 1), True)
         self.assertEqual(p.pred(0, 0), False)
 
+    def test_and_gate_numpy(self):
+        p = AndPerceptronNumpy()
+        self.assertEqual(p.pred(1, 1), True)
+        self.assertEqual(p.pred(1, 0), False)
+        self.assertEqual(p.pred(0, 1), False)
+        self.assertEqual(p.pred(0, 0), False)
+
+    def test_xor_gate(self):
+        p = XorPerceptronNumpy()
+        self.assertEqual(p.pred(1, 1), False)
+        self.assertEqual(p.pred(1, 0), True)
+        self.assertEqual(p.pred(0, 1), True)
+        self.assertEqual(p.pred(0, 0), False)
